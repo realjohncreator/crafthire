16fb316f70f8f1e813eeab14b2259d84be44864a
john <chukwuchidieberejohn@gmail.com>
2023-06-26 22:53:30 +0100

    Feat: Navbar UI

--- app/components/navbar/MenuOptions.tsx
+++ app/components/navbar/MenuOptions.tsx
@@ -1,15 +1,26 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 const MenuOptions = () => {
     return (
         <div className="">
-            <ul className="flex flex-row gap-4">
-                <li>Top 1%</li>
-                <li>Why?</li>
-                <li>Talents</li>
-                <li>About us</li>
-                <li>Help</li>
+            <ul className="lg:flex flex-row lg:gap-4 lg:text-lg lg:font-light text-xl lg:leading-normal leading-10">
+                <Link href=''>
+                    <li>Top 1%</li>
+                </Link>
+                <Link href=''>
+                    <li>Why?</li>
+                </Link>
+                <Link href=''>
+                    <li>Talents</li>
+                </Link>
+                <Link href=''>
+                    <li>About us</li>
+                </Link>
+                <Link href=''>
+                    <li>Help</li>
+                </Link>
             </ul>
         </div>
     );
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,14 +1,44 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import Logo from "@/app/components/navbar/Logo";
 import MenuOptions from "@/app/components/navbar/MenuOptions";
 import UserMenu from "@/app/components/navbar/UserMenu";
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai";
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [textColor, setTextColor] = useState("#A9A9B3");
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return (
         <div
             className="sticky top-0 flex justify-between items-center bg-[#00544B] text-[#FFFFFF] py-4 max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
-            <div className="flex flex-row items-center gap-20">
+            {/* Mobile Button */}
+            <div onClick={handleNav} className="block sm:hidden z-10">
+                {nav ? (
+                    <AiOutlineClose size={20} style={{color: `${textColor}`}}/>
+                ) : (
+                    <AiOutlineMenu size={20} style={{color: `${textColor}`}}/>
+                )}
+            </div>
+
+            {/* Mobile Menu */}
+            <div className={
+                nav
+                    ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center flex-col gap-4 items-center w-full h-screen bg-[#00544B] text-center ease-in duration-300"
+                    : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-[#00544B] text-center ease-in duration-300"
+            }>
+                <div className='pb-5'>
+                    <Logo/>
+                </div>
+                <MenuOptions/>
+            </div>
+
+            {/* Desktop Menu */}
+            <div className="lg:flex hidden flex-row items-center gap-20">
                 <Logo/>
                 <MenuOptions/>
             </div>
