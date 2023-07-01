73f2dcea39fff4769d065ad909d4d98e843ca297
john <chukwuchidieberejohn@gmail.com>
2023-07-01 14:32:33 +0100

    Feat: Partnership top talent ui

---
+++ app/components/partnership-top-talent/PartnershipTopTalent.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import PartnershipTopTalentText from "@/app/components/partnership-top-talent/PartnershipTopTalentText";
+import PartnershipTopTalentImage from "@/app/components/partnership-top-talent/PartnershipTopTalentImage";
+import Container from "@/app/components/Container";
+
+const PartnershipTopTalent = () => {
+    return (
+        <Container>
+            <div className="flex lg:flex-row flex-col items-center lg:gap-40 lg:pb-5">
+                <PartnershipTopTalentText/>
+                <PartnershipTopTalentImage/>
+            </div>
+        </Container>
+    );
+};
+
+export default PartnershipTopTalent;
