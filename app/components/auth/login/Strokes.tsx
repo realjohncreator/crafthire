818c9912923d928e1e24754a858cb5f780580779
john <chukwuchidieberejohn@gmail.com>
2023-07-08 16:56:17 +0100

    Login page UI

---
+++ app/components/auth/login/Strokes.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import arrow from "@/public/images/Arrow 22 (1).png";
+import Image from "next/image";
+
+const Strokes = () => {
+    return (
+        <div>
+            <Image src={arrow} alt=""/>
+        </div>
+    );
+};
+
+export default Strokes;
