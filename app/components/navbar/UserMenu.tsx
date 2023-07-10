1ebe7187c492c79038181a2a63841cd9f7a8ea7f
john <chukwuchidieberejohn@gmail.com>
2023-07-10 18:29:33 +0100

    code refactor

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,7 +7,7 @@ import HireTeamButton from "@/app/components/HireTeamButton";
 const UserMenu = () => {
     return (
         <div className="flex flex-row items-center gap-4">
-            <ThemeButton/>
+            {/*<ThemeButton/>*/}
             <Link href="pages/auth/login"
                   className="lg:font-bold hover:bg-[#00473F] lg:px-4 lg:py-3 md:px-2 md:py-2 px-4 py-3 rounded-sm transition duration-300">
                 Login
